ee8352c012c41fc634ef174fee5ee0b9a1241896
Michał Kopacz <michalkopacz.mk@gmail.com>
2019-03-20 13:45:02 +0100

    initialize

---
+++ src/main/java/com/mk/workshop/testing/confo/ConfoApplication.java
@@ -0,0 +1,13 @@
+package com.mk.workshop.testing.confo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfoApplication.class, args);
+    }
+
+}
